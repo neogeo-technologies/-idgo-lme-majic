9d4077a2545757cc67b5e22698990239230a1c06
leandro <leandroalmada86@gmail.com>
2021-09-20 11:36:41 +0200

    update email activation majic and fix index view lme_majic

--- idgo_lme_majic/views/__init__.py
+++ idgo_lme_majic/views/__init__.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
+
+from idgo_admin.exceptions import ExceptionsHandler
+from idgo_admin.exceptions import ProfileHttp404
+from idgo_admin.shortcuts import on_profile_http404
+
+from idgo_admin import LOGIN_URL
 
 from .majic import MajicCreate
 from .lme import LmeCreate
@@ -8,3 +19,8 @@ __all__ = [
     LmeCreate,
 ]
 
+@ExceptionsHandler(ignore=[Http404], actions={ProfileHttp404: on_profile_http404})
+@login_required(login_url=LOGIN_URL)
+@csrf_exempt
+def home(request, *args, **kwargs):
+    return redirect(reverse('idgo_admin:list_my_datasets'))
